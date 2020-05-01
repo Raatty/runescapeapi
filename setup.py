6fe7d74f5a866d50ddd4cd35bd6aad4908c6ade7
Raatty <raatty@noreply.github.com>
2020-05-01 19:04:27 +1200

    version 1

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open('readme.txt') as f:
     ld = f.read()
 setup(
 name='runescapeapi',
-version='0',
+version='1',
 description='grandexchange, highscores, beasts, runemetrics',
 long_description=ld,
+long_description_content_type="text/plain",
 author='raatty',
 py_modules=["runesapeapi"],
 install_requires=['wikia'],
