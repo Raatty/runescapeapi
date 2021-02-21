9a4c7eabb6ba7f14178ebc8b6d8ad55b3b88c4da
Raatty <me@raatty.club>
2021-02-21 17:08:31 +1300

    remove wiki support

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 with open('readme.txt') as f:
     ld = f.read()
 setup(
-name='runescapeapi',
-version='1',
-description='grandexchange, highscores, beasts, runemetrics',
-long_description=ld,
-long_description_content_type="text/plain",
-author='raatty',
-py_modules=["runesapeapi"],
-install_requires=['wikia'],
-python_requires='>=3',
-license='MIT',
-packages=find_packages()
+    name='runescapeapi',
+    version='1',
+    description='grandexchange, highscores, beasts, runemetrics',
+    long_description=ld,
+    long_description_content_type="text/plain",
+    author='raatty',
+    py_modules=["runesapeapi"],
+    install_requires=['requests'],
+    python_requires='>=3',
+    license='MIT',
+    packages=find_packages()
 )
